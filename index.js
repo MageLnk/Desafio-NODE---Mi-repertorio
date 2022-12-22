abbcccc2d5e2b01fef189fed353e32a5aa006bd1
MageLink <mage.link.zhezard@gmail.com>
2022-12-22 19:58:14 -0300

    Cors and webpage stablish. Now its crud time

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+// Basic starting points
 const app = express();
-
-app.get("/home", (req, res) => {
-  res.send("Hello World Express Js");
+const PORT = 5000;
+// Middleware
+//app.use(bodyParser.json());
+app.use(cors());
+// App
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
 });
 
-app.listen(3000, console.log("¡Servidor encendido!"));
+// Listener
+app.listen(PORT, () => {
+  console.log(`Server runing on port: htttp://localhost:${PORT}. Everything is fine`);
+});
